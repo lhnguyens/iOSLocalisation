be9504711b12ca3b2828f5cb34aa68e2dafe685a
Luan Nguyen <luan@Luans-MacBook-Pro.local>
2019-01-25 09:31:54 +0100

    First Commit

---
+++ iOSLocatlisation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLocatlisation
+//
+//  Created by Luan Nguyen on 2019-01-25.
+//  Copyright © 2019 Luan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
